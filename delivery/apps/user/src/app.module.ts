56e6ad23ab6029b84b65ccf448ea9fdaaa82ca30
Function <67085909+Function1790@users.noreply.github.com>
2026-03-10 16:02:58 +0900

    [Feat] App 필요 모듈 추가

--- delivery/apps/user/src/app.module.ts
+++ delivery/apps/user/src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.string().required(),
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config :ConfigService) => ({
+        type: 'postgres',
+        url: config.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true
+      }),
+      inject: [ConfigService]
+    }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
