6232410dedee4be8955f45fcbb15952e3b3fe0af
Function <67085909+Function1790@users.noreply.github.com>
2026-03-10 15:22:19 +0900

    [Feat] Auth 모듈 구성

---
+++ delivery/apps/user/src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  registerUser(token: string, @Body() registerDto: RegisterDto) {
+    if(token === null)
+      throw new UnauthorizedException();
+
+    return this.authService.register(token, registerDto);
+  }
+}
